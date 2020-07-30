cb9b82d8d6c209ec92658b693566fd74336e5be7
rakeshmollick <rakesmallik@gmail.com>
2020-07-30 23:42:08 +0600

    Done

---
+++ facto.js
@@ -0,0 +1,56 @@
+// 
+// console.log(typeof 4.5);
+// console.log(- (10 - 2));
+
+// console.log(3 > 2);//comparison
+// console.log("Aardvark" < "Zoroaster");
+// console.log("Itchy" != "Scratchy");
+
+// console.log(true && false);
+
+
+// console.log(false || true)//true
+// console.log(false || false);//false
+
+// console.log(null == undefined);
+// let mood = "light";
+// console.log(mood);//light
+
+// let luigisdebt = 140;
+// luigisdebt = luigisdebt - 35;
+// console.log(luigisdebt);
+
+// let one = 1, two = 2;
+// console.log(one+two);//3
+
+// var name = "Ayda";
+// const greetings = "Hello ";
+// console.log(greetings + name);
+
+
+// console.log(Math.max(2,4));
+// console.log(Math.min(45,36));
+// console.log(Math.max(200, 20000));
+// console.log(Math.min(200, 20000, 500, 50000, 36));
+
+//control flow
+
+// let theNumber = Number(prompt("Pick a number"));
+// console.log("Your number is the square root of " + theNumber * theNumber);
+
+// if (1 +1 ==2) console.log("it is true");
+
+// let number = 0;
+// while (number <= 120) {
+//     console.log(number);
+//     number = number + 25;
+
+// }
+let result = 1;
+let counter = 0;
+while (counter < 10){
+    result = result * 2;
+    counter = counter + 1;
+
+}
+console.log(result);
---
+++ max.js
@@ -0,0 +1,12 @@
+var business = 450;
+var minister = 350;
+var sochib = 550;
+if(business > minister){
+    if (business>sochib)
+    console.log("business>minister"){
+        console.log("Business is bigger");
+    }
+    console.log("minister>bigger"){
+        console.log("Business is bigger");
+    }
+}
